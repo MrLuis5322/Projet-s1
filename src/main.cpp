505b253edda9c404a2ed1acd96f4143271907995
MrLuis5322 <luis.lamontagne@gmail.com>
2026-09-24 10:29:29 -0400

    changement nom

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,149 @@
-/**
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+/*
+Projet: Le nom du script
+Equipe: 13
+Auteurs: Les membres auteurs du script
+Description: Breve description du script
+Date: 24/09/2026
+*/
 
-#include "Arduino.h"
+/*
+Inclure les librairies de functions que vous voulez utiliser
+*/
+#include <LibRobus.h>
+#include <Arduino.h>
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(13, OUTPUT);
+/*
+Variables globales et defines
+ -> defines...
+ -> L'ensemble des fonctions y ont acces
+*/
+
+bool bumperArr;
+int vertpin = 48;
+int rougepin = 49;
+bool vert = false;
+bool rouge = false;
+int etat = 0; // = 0 arrêt 1 = avance 2 = recule 3 = TourneDroit 4 = TourneGauche
+int etatPast = 0;
+float vitesse = 0.40;
+
+/*
+Vos propres fonctions sont creees ici
+*/
+
+void beep(int count){
+  for(int i=0;i<count;i++){
+    AX_BuzzerON();
+    delay(100);
+    AX_BuzzerOFF();
+    delay(100);  
+  }
+  delay(400);
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(37, HIGH);
+void arret(){
+  MOTOR_SetSpeed(RIGHT, 0);
+  MOTOR_SetSpeed(LEFT, 0);
+};
 
-  // wait for a second
-  delay(1000);
+void avance(){
+  MOTOR_SetSpeed(RIGHT,vitesse);
+  MOTOR_SetSpeed(LEFT, vitesse);
+};
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(13, LOW);
+void recule(){
+  MOTOR_SetSpeed(RIGHT, -0.5*vitesse);
+  MOTOR_SetSpeed(LEFT, -vitesse);
+};
 
-   // wait for a second
-  delay(1000);
+void tourneDroit(){
+  MOTOR_SetSpeed(RIGHT, 0.5*vitesse);
+  MOTOR_SetSpeed(LEFT, -0.5*vitesse);
+};
+
+void tourneGauche(){
+  MOTOR_SetSpeed(RIGHT, -0.5*vitesse);
+  MOTOR_SetSpeed(LEFT, 0.5*vitesse);
+};
+
+/*
+Fonctions d'initialisation (setup)
+ -> Se fait appeler au debut du programme
+ -> Se fait appeler seulement un fois
+ -> Generalement on y initilise les varibbles globales
+*/
+void setup(){
+  BoardInit();
+  
+  //initialisation
+  pinMode(vertpin, INPUT);
+  pinMode(rougepin, INPUT);
+  delay(100);
+  beep(3);
 }
-// kjhfdigfcvkjjvfj
+
+/*
+Fonctions de boucle infini
+ -> Se fait appeler perpetuellement suite au "setup"
+*/
+void loop() {
+  etatPast = etat;
+  bumperArr = ROBUS_IsBumper(3);
+  if (bumperArr){
+    if (etat == 0){
+      beep(2);
+      etat = 1;
+    } 
+    else{
+      beep(1);
+      etat = 0;
+    }
+  }
+  
+  vert = digitalRead(vertpin);
+  rouge = digitalRead(rougepin);
+  if (etat > 0){
+    if (vert && rouge){ // aucun obstacle => avance
+      etat = 1;
+    }
+    if (!vert && !rouge){  // obstacle devant => recule
+      etat = 2;
+    }
+    if (!vert && rouge){ // obstacle à gauche => tourne droit
+        etat = 3;
+      }
+    if (vert && !rouge){ // obstacle à droite => tourne gauche
+        etat = 4;
+    }
+  }
+
+  if (etatPast != etat){
+    arret();
+    delay(50);
+  }
+  else{
+    switch (etat)
+    {
+    case 0:
+      arret();
+      break;
+    case 1:
+      avance();
+      break;
+    case 2:
+      recule();
+      break;
+    case 3:
+      tourneDroit();
+      break;
+    case 4:
+      tourneGauche();
+      break;            
+    default:
+      avance();
+      etat = 1;
+    break;
+    }
+  }
+  delay(200);
+}
